7c7fb74d46b20991adcf0cc1cd3796de034a1f94
amitkumar1205-star <ak93068185@gmail.com>
2024-07-06 23:51:01 +0530

    New Pages Added

--- script.js
+++ script.js
@@ -10,3 +10,30 @@ document.addEventListener('DOMContentLoaded', () => {
         }
     });
 });
+
+$(document).ready(function(){
+  $('.slick_slider').slick({
+    slidesToShow: 1,
+    slidesToScroll: 1,
+    arrows: false,
+    // centerMode:true,
+    dots: true,
+    infinite: true,
+    autoplaySpeed: 2000,
+    autoplay: true,
+    responsive: [
+  {
+    breakpoint: 991,
+    settings: {
+      slidesToShow: 1,
+    }
+  },
+  {
+    breakpoint: 767,
+    settings: {
+      slidesToShow: 1,
+    }
+  }
+]
+  });
+});
